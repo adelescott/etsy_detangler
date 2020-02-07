4957330977d667c82a65747b4077b84c606f3ae7
Adele Scott <adele.frances.scott@gmail.com>
2020-02-07 18:54:34 +1100

    Improved error messages

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -45,8 +45,8 @@ object Main extends App {
           rfc.withHeader("Date", "Reference", "Payee", "Description", "Amount")
         )
       } catch {
-        case err: Exception => println(s"Error(s) detangling Etsy:\n${err.toString}")
+        case err: Exception => println(s"Error(s) detangling Etsy:\n${err.toString}\nNo output file written.")
       }
-    case Left(error) => println(s"Error(s) detangling Etsy: $error")
+    case Left(error) => println(s"Error(s) detangling Etsy: $error\nNo output file written.")
   }
 }
